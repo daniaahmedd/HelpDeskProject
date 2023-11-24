41b8e9dc2b05501d58a27b1d2ee8d0e012654b37
ghoniem123 <mohamedaymanqwerty@gmail.com>
2023-11-24 23:31:51 +0200

    update app 2

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,6 @@ app.use((req, res, next) => {
 });
 
 const db_url = `mongodb+srv://Mariam:LW7ZrU0N8A25kWqB@cluster0.qebr03m.mongodb.net/Software`;
-//const db_url = `mongodb://127.0.0.1:27017/${db_name}`; 
 
 const connectionOptions = {
   useUnifiedTopology: true,
