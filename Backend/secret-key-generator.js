94f37de1e69ac515ce51cde3b9a50bc951ba5468
ghoniem123 <mohamedaymanqwerty@gmail.com>
2023-11-29 21:14:58 +0200

    Update secret-key-generator.js

--- Backend/secret-key-generator.js
+++ Backend/secret-key-generator.js
@@ -4,4 +4,4 @@ const fs = require('fs');
 const Bytes = crypto.randomBytes(32);
 const secretKey = Bytes.toString('hex');
 
-fs.appendFileSyncFileSync('.env', `SECRET_KEY=${secretKey}`);
+fs.appendFileSync('.env', `SECRET_KEY=${secretKey}`);
