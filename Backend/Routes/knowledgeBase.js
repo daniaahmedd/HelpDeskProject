fe9921183e34abfa0be34c255c022e3fe558fb7c
Rawan Abdrabou <alaa.rawan2020@gmail.com>
2023-12-11 17:45:48 +0200

    updated knoleedgebase

--- Backend/Routes/knowledgeBase.js
+++ Backend/Routes/knowledgeBase.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const knowledgeBaseController = require("../controller/knowledgeBaseController");
-const authorizationMiddleware = require("../Middleware/authorizationMiddleware");
 
-router.get("/view", knowledgeBaseController.viewknowledgeBase);
-
-module.exports = router;
+module.exports = knowledgeBaseController;
--- Backend/controller/knowledgeBaseController.js
+++ Backend/controller/knowledgeBaseController.js
@@ -1,34 +1,53 @@
 const { subtle } = require("crypto");
 const knowledgeBaseModel = require("../Models/knowledgeBaseModel");
 
-
 const knowledgeBaseController = {
+  viewknowledgeBase: async (req, res) => {
+    try {
+      const allowedCategories = ["Software", "Hardware", "Network"];
+      const allowedSubCatogries = [
+        "Desktops",
+        "Laptops",
+        "Printers",
+        "Servers",
+        "Networking equipment",
+        "Operating system",
+        "Application software",
+        "Custom software",
+        "Integration issues",
+        "Email issues",
+        "Internet connection problems",
+        "Website errors",
+      ];
+      // const items = await knowledgeBaseModel.find().lean().exec();
+      // return res.json({items})
+      
+      const { question_category, question_subcategory } = req.body;
+      if (question_category==null&&question_subcategory==null) {
+        const data = await knowledgeBaseModel.find().lean().exec();
+        return res.status(200).json(data);
+      }
+      
+      if (allowedCategories.includes(question_category) && allowedSubCatogries.includes(question_subcategory)) {
+          const data = await knowledgeBaseModel.find({question_category,question_subcategory}).lean().exec();
+          return res.status(200).json(data);
+        } 
+        if(allowedCategories.includes(question_category)&& !allowedSubCatogries.includes(question_subcategory) ){
+          const data = await knowledgeBaseModel.find({question_category}).lean().exec();
+          return res.status(200).json(data);
+        }
+        if(!allowedCategories.includes(question_category)&& allowedSubCatogries.includes(question_subcategory) ){
+          const data = await knowledgeBaseModel.find({question_subcategory}).lean().exec();
+          return res.status(200).json(data);
+        }
+ 
 
-  viewknowledgeBase:async(req,res)=>{
-  try{
-    const { Category, Subcategory } = req.body;
-    if (!allowedCategories.includes(Category)) {
-      const data = await knowledgeBaseModel.find();
-      return res.status(200).json(data);
+       } catch (e) {
+      return res.status(500).json({ message: e.message });
     }
-    if (!allowedSubCatogries.includes(Subcategory)){
-      if(allowedCategories.includes(Category)){
-      const data = await knowledgeBase.find(Category);
-      return res.status(200).json(data);
 
-    }
-    else{
-      const data = await knowledgeBase.find();
-      return res.status(200).json(data);
-
-    }
+    // Check if category is null
   
   }
-  } catch (e) {
-    return res.status(500).json({ message: e.message });
-  }
-
-  // Check if category is null
-},
 };
 module.exports = knowledgeBaseController;
