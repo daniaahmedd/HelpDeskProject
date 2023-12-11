e4e80a4a8e579b636b4126b53d2a12826b1818e0
Rawan Abdrabou <alaa.rawan2020@gmail.com>
2023-12-11 17:53:02 +0200

    Update authenticationMiddleware.js

--- Backend/Middleware/authenticationMiddleware.js
+++ Backend/Middleware/authenticationMiddleware.js
@@ -3,21 +3,21 @@ require('dotenv').config();
 const secretKey =process.env.SECRET_KEY ;
 
 module.exports = function authenticationMiddleware(req, res, next) {
-  // const cookie = req.cookies;
-  // if (!cookie) {
-  //   return res.status(401).json({ message: "No Cookie provided" });
-  // }
-  // const token = cookie.token;
-  // if (!token) {
-  //   return res.status(405).json({ message: "No token provided" });
-  // }
+  const cookie = req.cookies;
+  if (!cookie) {
+    return res.status(401).json({ message: "No Cookie provided" });
+  }
+  const token = cookie.token;
+  if (!token) {
+    return res.status(405).json({ message: "No token provided" });
+  }
 
-  // jwt.verify(token, secretKey, (error, decoded) => {
-  //   if (error) {
-  //     return res.status(403).json({ message: "Invalid token" });
-  //   }
+  jwt.verify(token, secretKey, (error, decoded) => {
+    if (error) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
         
-  //   req.user = decoded.user;
+    req.user = decoded.user;
   next();
-  // });
+  });
 };
