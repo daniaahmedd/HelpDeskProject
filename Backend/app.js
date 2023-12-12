4416e19b7f7a4f730b4d0e710f25db63a5b6c671
rawan <rawanwaelsaad2017@gmail.com>
2023-12-12 10:48:51 +0200

    .

--- Backend/app.js
+++ Backend/app.js
@@ -25,7 +25,7 @@ app.use(
 );
 
 //const db_url = `mongodb+srv://Mariam:LW7ZrU0N8A25kWqB@cluster0.qebr03m.mongodb.net/Software`;
-// const db_url = 'mongodb://127.0.0.1:27017/Helpdesk';
+const db_url = 'mongodb://127.0.0.1:27017/Helpdesk';
 
 
 const connectionOptions = {
