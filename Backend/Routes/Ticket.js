c8fac780fc38ce1e1830e19df310607f0ade2687
rawan <rawanwaelsaad2017@gmail.com>
2023-12-20 00:03:02 +0200

    .

--- Backend/Routes/Ticket.js
+++ Backend/Routes/Ticket.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const authorizationMiddleware = require("../Middleware/authorizationMiddleware");
 const TicketController = require("../controller/TicketController");
+//const ReportController = require('<path-to>/ReportController');
 
 
 router.post("/", TicketController.createTicket);
-router.put("/:ticketid", TicketController.updateTicket);
-router.post("/update/:ticketId",authorizationMiddleware(['Agent']), ReportController.createReport)
+//router.put("/:ticketid", TicketController.updateTicket);
+//router.post("/update/:ticketId",authorizationMiddleware(['Agent']), ReportController.createReport)
 
 router.put("/rate/:ticketid", TicketController.rateTicket);
 //router.get
