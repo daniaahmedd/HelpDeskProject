dcaae71d5f5f195c57bba9da5a3e45c244a6029a
daniaahmedd <121130141+daniaahmedd@users.noreply.github.com>
2023-12-23 21:38:11 +0200

    update Frontend home

--- Frontend/src/components/homeBody.jsx
+++ Frontend/src/components/homeBody.jsx
@@ -1,27 +1,34 @@
 import React from "react";
-import '../stylesheets/homePage.css'
+import "../stylesheets/homePage.css"
 import "../stylesheets/homePageBackground.scss";
-import logo from '../assets/Projectlogo.png'; // Tell Webpack this JS file uses this image
-
-
+import logo from '../assets/Projectlogo.png';
+import Login from "../components/login";
 
 export default function HomeBody() {
     return(
         <>
         <div className='home_body'>
-            <div className="introSection">
+            <div className="introSection" style={{display:"inline-block"}}>
                 <img src={logo} style={{width:"90px", marginLeft:"15px", marginTop:"15px"}}/>
                 <div className="introSecText">
                     <h2 className="homeTitle" style={{color:'white', marginTop:"10px", fontFamily:"Roborto_Slab"}}>Helpdesk</h2>
                     <h3 style={{color:'white',fontWeight: "light", fontFamily:"Roborto_Slab", marginBottom:"25px"}}>
-                        Support your external customers or maintain a <br/> service desk for internal users with helpdesk <br/> projects
+                        Support your external customers or maintain a service desk for internal users with helpdesk projects
                     </h3>
                     <h6 style={{color:'white',fontFamily:"Roborto_Slab"}}>
                         Unlimited reporters and 3 agents for free. Cloud or server – Get started now!
                     </h6>
-                    <button type="button" class="btn btn-outline-secondary" className="introSecButton">Get Started</button>
+                    <button type="button" class="btn btn-outline-secondary introSecButton "><span style={{color:'white'}}>Get Started</span></button>
                 </div>
-                
+            </div>
+            <div className="loginSection" style={{display:"inline-block"}}>
+                <div className="loginSecText">
+                    <h2 className="homeTitle" style={{color:'white', marginTop:"47px", fontFamily:"Roborto_Slab", textAlign:"center"}}>Let our Helpdesk streamline your customer support service</h2>
+                    <h3 style={{color:'white', fontWeight: "light", fontFamily:"Roborto_Slab", marginBottom:"25px", marginTop:"20px"}}>
+                        Get the essentials to organize your support or service desk processes in helpdesk and save your team the time and effort of switching context – support tickets are easily managed next to other projects in your organization.
+                    </h3>
+                    <button type="button" href='' class="btn btn-outline-secondary introSecButton btn-lg" style={{width:'45%', textAlign:"center", marginLeft:'25%'}}><span style={{color:'white'}}>Login Now</span></button>
+                </div>
             </div>
         </div>
         </>
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import Navbar from "../components/navbar";
 import HomeBody from "../components/homeBody";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "../stylesheets/homePage.css";
+import "../stylesheets/homePage.css";
 import "../stylesheets/homePageBackground.scss";
 
 export default function HomePage() {
 
     return (
         <>
-          <div style={{}}>
+          <div>
             <Navbar />
             <HomeBody />
           </div>
