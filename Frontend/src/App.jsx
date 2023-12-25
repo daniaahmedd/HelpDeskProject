2e7f70ac86933b7a8af30788c74aa8b174d2d1d6
Mariam-mimi7 <mariamokby@gmail.com>
2023-12-25 02:19:56 +0200

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,10 @@ import Knowldgebasess from "./pages/knowldgebases";
 import WorkFlowPage from "./pages/WorkFlowPage";
 import ReportPage from "./pages/ReportPage";
 import Ticket from "./pages/Ticket";
-
+import StartRestore from '../components/restore';
+import Customization from '../components/CustomizationCheck';
+import Check from './CustomizationForm';
+import UpdateCustomization from './UpdateCustomization';
 function App() {
     return (
         <>
@@ -15,6 +18,11 @@ function App() {
                 <Route path="/workflow" element={<WorkFlowPage />} />
                 <Route path="/report" element={<ReportPage />} />
                 <Route path="/Ticket" element={<Ticket />} />
+                <Route path="/restore" element={<StartRestore />} />
+                <Route path="/CustomizationForm" element={<Check />} />
+                <Route path="/UpdateCustomization" element={<UpdateCustomization />} />
+                <Route path="/CustomizationCheck" element={<Customization />} />
+
             </Routes>
         </>
     );
