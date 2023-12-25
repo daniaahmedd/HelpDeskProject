5481ed263618ce889a23aa4b118e27f8eca0eec1
Mariam-mimi7 <mariamokby@gmail.com>
2023-12-25 02:20:36 +0200

    Update customization.js

--- Backend/Routes/customization.js
+++ Backend/Routes/customization.js
@@ -1,16 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const CustomizationController = require("../controller/CustomizationController");
-const authorizationMiddleware = require("../Middleware/authorizationMiddleware");
+const CustomizationController = require('../controller/CustomizationController');
+const authorizationMiddleware = require('../Middleware/authorizationMiddleware');
 
 
 
 
 
-// * create eCustomization
-router.post("/styleCustomize", authorizationMiddleware(['admin']),CustomizationController.createCustomization);
+// POST /customization/styleCustomize
+router.post('/styleCustomize' ,authorizationMiddleware(['admin']),CustomizationController.createCustomization);
 
-//* Update Customization
-router.put("/styleCustomize/edit/:customizationId", authorizationMiddleware(['admin']),CustomizationController.updateCustomization);
+// PUT /customization/styleCustomize/edit
+router.put('/styleCustomize/edit', authorizationMiddleware(['admin']),CustomizationController.updateCustomization);
+
+router.get('/styleCustomize/find' ,authorizationMiddleware(['admin']),CustomizationController.getCustomization);
 
 module.exports = router; 
