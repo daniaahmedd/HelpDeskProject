19d7de0f127ff2405a11f6c741566aa4bdb64baf
Mariam-mimi7 <mariamokby@gmail.com>
2023-12-25 02:22:24 +0200

    Create RestorePage.jsx

---
+++ Frontend/src/pages/RestorePage.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../stylesheets/restore.css";
+
+
+export default function RestorePage() {
+
+    return (
+        <>
+          <div >
+            <Navbar />
+            
+          </div>
+        </>
+    );
+
+}
