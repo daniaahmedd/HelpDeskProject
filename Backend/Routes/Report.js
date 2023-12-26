770f614e99b5baf88ebf26a39c09ebf970d71d48
sama2000 <105825758+sama2000@users.noreply.github.com>
2023-12-26 02:53:58 +0200

    final frontendreport

--- Backend/Routes/Report.js
+++ Backend/Routes/Report.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const ReportController = require("../controller/ReportController");
+const authorizationMiddleware = require("../Middleware/authorizationMiddleware");
 
 
 
-router.post("/create", ReportController.gettickets)
-router.post("/create/:ticketId", ReportController.createReport)
-router.post("/view", ReportController.getReport)
-router.get("/viewTrends", ReportController.getTrends)
-router.get("/tickets",ReportController.getTicket)
-//authorizationMiddleware(['Manager'])
+router.post("/create",authorizationMiddleware(['Manager']) ,ReportController.gettickets)
+router.post("/create/:ticketId", authorizationMiddleware(['Manager']),ReportController.createReport)
+router.post("/view", authorizationMiddleware(['Manager']),ReportController.getReport)
+router.get("/viewTrends", authorizationMiddleware(['Manager']),ReportController.getTrends)
+router.get("/tickets",authorizationMiddleware(['Manager']),ReportController.getTicket)
+
 module.exports = router;
